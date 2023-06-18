4bcf1ed62a6e76dd3e36113fc481d81627019934
Arhin-Eben <yawebenezer918@gmail.com>
2023-06-17 19:08:25 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry to the program,this is where execution starts
+ * Description - program that prints all possible combinations of single-digit
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
