da51bc9b25114651fedc33a510d09b30a506220b
Arhin-Eben <yawebenezer918@gmail.com>
2023-06-21 01:50:09 +0000

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,14 +6,15 @@
  *
  * Return: 1 if c is lowercase, 0 otherwise.
  */
+
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		 return (1);
+		return (1);
 	}
 	else
 	{
-		 return (0);
+		return (0);
 	}
 }
