747be09864b095bb3c50872650697a479ee85190
Arhin-Eben <yawebenezer918@gmail.com>
2023-06-21 03:04:41 +0000

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,7 +17,7 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-        	else if (n < 0)
+        else if (n < 0)
 	{
 		 _putchar(45);
 		 return (-1);
@@ -25,6 +25,7 @@ int print_sign(int n)
 	else
 	{
 		_putchar(48);
+
 		return (0);
 	}
 	_putchar('\n');
