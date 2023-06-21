61247af528a4b8b3c2a27a368134c7e6aa20dfaa
Arhin-Eben <yawebenezer918@gmail.com>
2023-06-21 23:05:56 +0000

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,12 +11,12 @@
 
 int _abs(int i)
 {
-        if (i < 0)
-        {
-                return (i * -1);
-        }
-        else
-        {
-                return (i);
-        }
+	if (i < 0)
+	{
+		return (i * -1);
+	}
+	else
+	{
+		return (i);
+	}
 }
