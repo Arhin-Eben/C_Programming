47de2e8a154789e0ae443e3070978c93b078b236
Arhin-Eben <yawebenezer918@gmail.com>
2023-06-30 05:24:04 +0000

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,14 +2,21 @@
 /**
 *main - Entry point
 *
-*Return: Always 0 (success)
+*Return: always 0 (success)
 */
+
 int main(void)
 {
-	int a[] = {97, 98, 99};
-	int *p = a + 2;
+	int n;
+	int a[5];
+	int *p;
 
-	printf("a[2] = %dn", *(p - 1) + 1);
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) = 98;
 
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,9 @@
-char *_strcat(char *dest,const char *src);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
@@ -8,4 +13,4 @@ char *leet(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
+#endif
