630092034c65a23ba2bac8d3087d5accf790d947
Arhin-Eben <yawebenezer918@gmail.com>
2023-08-30 03:00:24 +0000

    annother

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,9 +1,11 @@
+#include "lists.h"
+
 /**
-*listint_len - returns the number of elements in a linked lists
-*@h: linked list of type listint_t to traverse
-*
-*Return: number of nodes
-*/
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
 size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
@@ -13,5 +15,6 @@ size_t listint_len(const listint_t *h)
 		num++;
 		h = h->next;
 	}
+
 	return (num);
 }
